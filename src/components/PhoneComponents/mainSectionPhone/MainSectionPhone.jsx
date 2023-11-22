01745b517123c069a226c0a59de509edd5207408
Пользователь <example@mail.com>
2023-11-22 21:08:37 +0300

    добавил моб секцию №5

---
+++ src/components/PhoneComponents/mainSectionPhone/MainSectionPhone.jsx
@@ -0,0 +1,13 @@
+import styles from "./MainSectionPhone.module.sass"
+
+function MainSectionPhone() {
+    return (
+        <div className={styles.mainPhone}>
+            <div className={styles.mainPhoneContainer}>
+                
+            </div>
+        </div>
+    )
+}
+
+export default MainSectionPhone;
