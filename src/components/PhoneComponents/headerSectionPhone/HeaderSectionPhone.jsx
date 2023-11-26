48b30b668cef6d801d62b1420927dbdf49c41707
Пользователь <example@mail.com>
2023-11-27 02:04:09 +0300

    меняю моб.версию полностью #12

--- src/components/PhoneComponents/headerSectionPhone/HeaderSectionPhone.jsx
+++ src/components/PhoneComponents/headerSectionPhone/HeaderSectionPhone.jsx
@@ -3,6 +3,7 @@
 import { useState, useEffect } from "react";
 import ModalNav from "../modal/ModalNav";
 import styles from "./HeaderSectionPhone.module.sass";
+import { LuMail } from "react-icons/lu";
 
 export default function HeaderSectionPhone() {
   const [modalNavOn, setModalNavOn] = useState(false);
@@ -13,7 +14,6 @@ export default function HeaderSectionPhone() {
     }, 300);
   };
 
-
   const telegramChannelUrl = "https://t.me/+79189255217";
   const whatsAppChannelUrl = "https://wa.me/+79189255217";
 
@@ -22,18 +22,20 @@ export default function HeaderSectionPhone() {
       <div className={styles.headerPhoneContainer}>
         <div className={styles.headerPhoneContent}>
           <div className={styles.headerPhoneLogo}>
-            <img src="/headerimg/logoBlack.svg" alt="#" width={50}/>
+            <img src="/headerimg/logoSecond.svg" alt="#" />
             <p>СТРОИТЕЛЬНАЯ КОМПАНИЯ</p>
           </div>
           <div className={styles.headerPhoneEmail}>
-            <img src="/headerimg/mail.svg" alt="#" />
+            <div className={styles.headerPhoneEmailImg}>
+              <LuMail width={55} />
+            </div>
             <div className={styles.headerPhoneEmailText}>
               <p>Email:</p>
               <p>grupp.trio@bk.ru</p>
             </div>
           </div>
           <div className={styles.headerPhoneCall}>
-            <img src="/headerimg/call.svg" alt="#" />
+            <img src="/headerimg/phoneSecond.svg" alt="#" />
             <div className={styles.headerPhoneCallText}>
               <p>+7(000) 000-00-00</p>
               <p>+7(000) 000-00-00</p>
